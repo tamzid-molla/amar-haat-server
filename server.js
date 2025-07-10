9aff1135fcf6ac795c1b65e3d4c8d4aabdbbb744
TAMZID <tamzid2233@gmail.com>
2025-07-11 00:39:16 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 3001;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//MongoDb connection
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`server running at ${port}`);
+});
